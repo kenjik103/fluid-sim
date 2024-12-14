c5d49bf87445857a7a2bd80e2f39cb399c161eff
Kenjiro Kanagy <kenjik103@gmail.com>
2024-12-14 14:26:37 -0800

    Initial commit. Basic build system and blank window up and running

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "raylib.h"
+
+int main(void) {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+
+  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+  // Main game loop
+  while (!WindowShouldClose()) // Detect window close button or ESC key
+  {
+    DrawFPS(GetMouseX(), GetMouseY());
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    DrawText("wawawwaawlkdjflkajdf", 190, 200, 20,
+             LIGHTGRAY);
+
+    EndDrawing();
+  }
+
+  // De-Initialization
+  CloseWindow(); 
+
+  return 0;
+}
